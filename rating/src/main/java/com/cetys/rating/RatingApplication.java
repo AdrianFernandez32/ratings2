7df191c5abaa50aa9a7d88ea12d2a5b811ee1717
MrJ8585 <josea.solis@cetys.edu.mx>
2023-12-03 15:43:17 -0800

    Inicializando el proyecto

---
+++ rating/src/main/java/com/cetys/rating/RatingApplication.java
@@ -0,0 +1,13 @@
+package com.cetys.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingApplication.class, args);
+	}
+
+}
