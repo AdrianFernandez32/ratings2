a87e8b2f45f6a0f64f49e599cc0184656c964f92
Caleb Payán Inzunza <calebpayan75@gmail.com>
2023-12-03 16:33:32 -0800

    controller mvc file

--- rating/src/main/java/com/cetys/rating/DAO/Entities/RestroomEntity.java
+++ rating/src/main/java/com/cetys/rating/DAO/Entities/RestroomEntity.java
@@ -1,4 +1,4 @@
-package com.cetys.rating.dao.entities;
+package com.cetys.rating.DAO.Entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- rating/src/main/java/com/cetys/rating/DAO/Services/RatingService.java
+++ rating/src/main/java/com/cetys/rating/DAO/Services/RatingService.java
@@ -3,7 +3,6 @@ package com.cetys.rating.DAO.Services;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cetys.rating.DAO.Entities.RatingEntity;
